0d9ee4c32d1c8d1dac36ea5e2251d11fabd0bef5
Renan Souza <renantrsouza@gmail.com>
2021-06-30 13:49:27 -0400

    passei os dados pelo construtor

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ import {Cliente} from "./Cliente.js"
 import { ContaCorrente} from "./ContaCorrente.js"
 
 //Cliente 1
-const cliente1 = new Cliente();
+const cliente1 = new Cliente("Ricardo", 11122233309);
 const conta1 = new ContaCorrente();
 conta1.cliente = cliente1;
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
 conta1.agencia = 101;
 conta1.depositar(200);
-console.log(conta1._saldo);
+//console.log(conta1._saldo);
 
 //Cliente 2
-const cliente2 = new Cliente();
+const cliente2 = new Cliente("Alice", 88822233309);
 const conta2 = new ContaCorrente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
 conta2.cliente = cliente2;
 conta2.agencia = 102;
 conta2.depositar(50);
