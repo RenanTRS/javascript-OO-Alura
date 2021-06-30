5b85d435a6d81a20d4de418f749c2424cc17cdfa
Renan Souza <renantrsouza@gmail.com>
2021-06-30 13:57:35 -0400

    comentario

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const cliente2 = new Cliente("Alice", 88822233309);
 const conta2 = new ContaCorrente(102, cliente2);
 conta2.depositar(50);
 
-
+//Output
 conta1.transfere(50, conta2);
 console.log(conta1);
